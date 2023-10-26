941a0fe42fe619411bb1a3095fe012400f9a02ac
jon <foursquarerc@gmail.com>
2023-10-26 10:15:10 -0600

    mid

--- src/globals.cpp
+++ src/globals.cpp
@@ -19,7 +19,7 @@ Drive chassis (
 
   // Wheel Diameter (Remember, 4" wheels are actually 4.125!)
   //    (or tracking wheel diameter)
-  ,2.75
+  ,3.25
 
   // Cartridge RPM
   //   (or tick per rotation if using tracking wheels)
@@ -29,7 +29,7 @@ Drive chassis (
   //    (or gear ratio of tracking wheel)
   // eg. if your drive is 84:36 where the 36t is powered, your RATIO would be 2.333.
   // eg. if your drive is 36:60 where the 60t is powered, your RATIO would be 0.6.
-  ,1.25
+  ,1.66
 
   // Uncomment if using tracking wheels
   /*
